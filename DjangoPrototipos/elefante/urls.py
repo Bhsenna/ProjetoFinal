61b0cc2b863a4de77752d5cb07e6ec149b0f2f46
Bhsenna <57589013+Bhsenna@users.noreply.github.com>
2022-09-20 14:00:43 -0300

    Pagina antiga do Interact.js apagada

--- DjangoPrototipos/elefante/urls.py
+++ DjangoPrototipos/elefante/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tela),
-    path('movel', views.movel),
+    path('', views.movel),
 ]
--- DjangoPrototipos/elefante/views.py
+++ DjangoPrototipos/elefante/views.py
@@ -3,9 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def tela(request):
-    return render(request, 'elefante/QuestionType1.html')
-
-
 def movel(request):
     return render(request, 'elefante/index.html')
